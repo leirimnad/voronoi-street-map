f71cb9874b2b75477566a31e3da338c465339cf2
Daniil Myronov <myrondan@fit.cvut.cz>
2022-12-03 14:47:28 +0100

    page setup

--- voronoiMap.js
+++ voronoiMap.js
@@ -1,4 +1,15 @@
 "use strict";
 
 let canvas = document.getElementById("voronoiMap");
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+
+window.addEventListener("resize", function() {
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+});
+
 let ctx = canvas.getContext("2d");
+
+import Voronoi from "./rhill-voronoi-core.js";
+
