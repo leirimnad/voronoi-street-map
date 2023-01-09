d978f388b183ef0a764ecb6e6463c98a0276336c
Daniil Myronov <myrondan@fit.cvut.cz>
2023-01-09 19:12:46 +0100

    change library to d3-delaunay

--- voronoiMap.js
+++ voronoiMap.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import {random, seed} from "./random.js";
+import {Delaunay} from "https://cdn.skypack.dev/d3-delaunay@6";
+
 let canvas = document.getElementById("voronoiMap");
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
@@ -10,10 +13,6 @@ window.addEventListener("resize", function() {
 });
 
 let ctx = canvas.getContext("2d");
-let voronoi = new Voronoi();
-
-import Voronoi from "./rhill-voronoi-core.js";
-import {seed, random} from "./random.js";
 
 class StreetLevel {
     constructor(nSites, lineColor="black", lineWidth=1) {
@@ -26,66 +25,47 @@ class StreetLevel {
 function drawFiniteMap(canvas, ctx, seedValue, levels) {
     seed(seedValue);
 
-    let accumulatedCells = generateCanvasCell(canvas);
+    let accumulatedCells = [[[0,0], [canvas.width, 0], [canvas.width, canvas.height] , [0, canvas.height]]];
 
     for (let level of levels) {
-        let cells = drawLevel(canvas, ctx, level, accumulatedCells);
-        accumulatedCells = cells;
+        accumulatedCells = drawLevel(canvas, ctx, level, accumulatedCells);
     }
-}
-
-function generateCanvasCell(canvas) {
-    let cell = new Voronoi.prototype.Cell();
-
-    cell.halfedges = [
-        {getStartpoint: function() {return {x: canvas.width, y: 0}}, getEndpoint: function() {return {x: 0, y: 0}}},
-        {getStartpoint: function() {return {x: 0, y: 0}}, getEndpoint: function() {return {x: 0, y: canvas.height}}},
-        {getStartpoint: function() {return {x: 0, y: canvas.height}}, getEndpoint: function() {return {x: canvas.width, y: canvas.height}}},
-        {getStartpoint: function() {return {x: canvas.width, y: canvas.height}}, getEndpoint: function() {return {x: canvas.width, y: 0}}}
-    ];
-
-    cell.site = {x: canvas.width / 2, y: canvas.height / 2};
-
-    return [cell];
 }
 
 function drawLevel(canvas, ctx, level, cells) {
     let newCells = [];
 
-    if (level.lineColor != "random")
+    if (level.lineColor !== "random")
         ctx.strokeStyle = level.lineColor;
 
     for (let cell of cells) {
 
-        if (level.lineColor == "random")
+        if (level.lineColor === "random")
             ctx.strokeStyle = "#"+Math.floor(random()*16777215).toString(16);
         ctx.lineWidth = 1;
 
         let sites = generateSites(cell, level.nSites);
+        let delaunay = Delaunay.from(sites)
+        let voronoi = delaunay.voronoi(getBoundingBox(cell));
 
-        let bbox = getBoundingBox(cell);
-        let diagram = voronoi.compute(sites, bbox);
-
-
-        newCells = newCells.concat(diagram.cells);
+        let generatedCells = [...voronoi.cellPolygons()];
+        newCells = newCells.concat(generatedCells);
 
         for (let site of sites) {
             ctx.beginPath();
-            ctx.arc(site.x, site.y, 2, 0, 2 * Math.PI);
+            ctx.arc(site[0], site[1], 2, 0, 2 * Math.PI);
             ctx.stroke();
         }
 
         ctx.lineWidth = level.lineWidth;
-        for (let edge of diagram.edges) {
-            // if (edge.rSite == null) continue;
-            let p1 = edge.va;
-            let p2 = edge.vb;
-            if (p1 && p2) {
-                ctx.beginPath();
-                ctx.moveTo(p1.x, p1.y);
-                ctx.lineTo(p2.x, p2.y);
-                ctx.stroke();
+        for (let cell of generatedCells) {
+            ctx.beginPath();
+            ctx.moveTo(cell[0][0], cell[0][1]);
+            for (let i = 1; i < cell.length; i++) {
+                ctx.lineTo(cell[i][0], cell[i][1]);
             }
+            ctx.closePath();
+            ctx.stroke();
         }
     }
 
@@ -98,45 +78,56 @@ function generateSites(cell, nSites) {
         let bbox = getBoundingBox(cell);
         let x, y;
         do {
-            x = random() * (bbox.xr - bbox.xl) + bbox.xl;
-            y = random() * (bbox.yb - bbox.yt) + bbox.yt;
-        } while (!isInsideCell({x: x, y: y}, cell));
-        let site = {x: x, y: y};
-        sites.push(site);
+            x = Math.round(random() * (bbox[2] - bbox[0]) + bbox[0]);
+            y = Math.round(random() * (bbox[3] - bbox[1]) + bbox[1]);
+        } while (!isInsidePolygon([x, y], cell, false) && random() < 0.99);
+        sites.push([x, y]);
     }
     return sites;
 }
 
 function getBoundingBox(cell) {
-    let bbox = Voronoi.prototype.Cell.prototype.getBbox.call(cell);
+    let xs = cell.map(p => p[0]);
+    let ys = cell.map(p => p[1]);
+    return [Math.min(...xs),
+            Math.min(...ys),
+            Math.max(...xs),
+            Math.max(...ys)]
+}
 
-    return {
-        xl: bbox.x,
-        xr: bbox.x + bbox.width,
-        yt: bbox.y,
-        yb: bbox.y + bbox.height
+function isInsidePolygon(point, polygon, includeEdges=true) {
+    let correctSide = null;
+    let nVertices = polygon.length;
+    for (let i = 0; i < nVertices - 1; i++) {
+        let p1 = polygon[i];
+        let p2 = polygon[i + 1];
+        if (p1[0] === p2[0] && p1[1] === p2[1])
+            continue;
+        let edgeVector = [p2[0] - p1[0], p2[1] - p1[1]];
+        let pointVector = [point[0] - p1[0], point[1] - p1[1]];
+        let side = Math.sign(edgeVector[0] * pointVector[1] - edgeVector[1] * pointVector[0]);
+        if (side === 0)
+            return includeEdges && isOnEdge(point, p1, p2);
+        if (correctSide == null)
+            correctSide = side;
+        if (side !== correctSide)
+            return false;
     }
+    return true;
 }
 
-function isInsideCell(point, cell, includeEdges=true){
-    let res = cell.__proto__.pointIntersection.call(cell, point.x, point.y);
-    if (res == 1) return true;
-    if (res == 0 && includeEdges) return true;
-    return false;
-}
-
-function isOnEdge(point, p1, p2) {
+function isOnEdge(p, p1, p2) {
     let dx = p2.x - p1.x;
     let dy = p2.y - p1.y;
     let d = Math.sqrt(dx * dx + dy * dy);
-    let d1 = Math.sqrt((point.x - p1.x) * (point.x - p1.x) + (point.y - p1.y) * (point.y - p1.y));
-    let d2 = Math.sqrt((point.x - p2.x) * (point.x - p2.x) + (point.y - p2.y) * (point.y - p2.y));
-    return (d1 + d2) == d;
+    let d1 = Math.sqrt((p.x - p1.x) * (p.x - p1.x) + (p.y - p1.y) * (p.y - p1.y));
+    let d2 = Math.sqrt((p.x - p2.x) * (p.x - p2.x) + (p.y - p2.y) * (p.y - p2.y));
+    return (d1 + d2) === d;
 }
 
 
 let seedValue = Date.now();
-let levels = [new StreetLevel(14, "black", 6), new StreetLevel(4, "random", 2)];
+let levels = [new StreetLevel(14, "black", 6), new StreetLevel(8, "random", 2)];
 drawFiniteMap(canvas, ctx, seedValue, levels);
 
 
